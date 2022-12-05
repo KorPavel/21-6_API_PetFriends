82bbd6d40b91aa74cb8ca82cffb4c1869fb85eba
KorPavel <kor-p@bk.ru>
2022-12-05 16:46:07 +0300

    Update settings.py
    Добавлены два элемента:
    - log_path - для создания папки, в которую будут помещаться отчёты по проведённым тестам
    - log_file - для создания файла отчётов (каждый день новый файл)

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 import os
+from os import path
+from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,3 +9,6 @@ valid_email = os.getenv('valid_email')
 valid_password = os.getenv('valid_password')
 invalid_email = '55555@mail.ru'
 invalid_password = '55555'
+# ------------------------------
+log_path = path.join(path.dirname(__file__), 'logs').replace('\\', '/')
+log_file = f'log_{datetime.now().strftime("%Y%m%d")}.txt'
